869ed825c8c2e8fd15134fe4fcf62dd2c437bc08
+ <+>
2023-03-11 13:48:51 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            'Accept': "application/json",
+        },
+        body: JSON.stringify({
+            name: 'Steve',
+            email: 'steve@steve.com',
+        })
+
+    })
+        .then(function (res) {
+            return res.json()
+        })
+        .then(function (user) {
+            console.log(user)
+            document.body.append(user.id)
+            document.body.append(user.email)
+        })
+        .catch(function(error) {
+            alert("request to http://localhost:3000/users failed, reason: Unauthorized Access");
+            console.log(error.message)
+            document.body.append(error.message)
+        })
+    }
+fetch("http://localhost:3000/users", submitData)
